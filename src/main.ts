525d64b6a3b85ae1d807582f5f501841f7555b51
Mr_Whoopee <artem.holovko.97@gmail.com>
2025-12-08 14:50:37 +0200

    ts1

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+// let a: unknown;
+// a = 5;
+// if(typeof a === "string") a.toUpperCase();
+
+// interface Car {
+//   engine: string;
+//   color: string;
+//   weight?: number;
+//   readonly number: string;
+// }
+
+// const car: Car = {
+//   engine: "v8",
+//   color: "red",
+//   weight: 2000,
+//   number: "ac2020",
+// };
+
+// const car1: Car = {
+//   engine: "v4",
+//   color: "red",
+//   number: "ac2025",
+// };
+
+// car.color = "blue";
