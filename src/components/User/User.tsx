dc7732b82db6b6231b76b9dc96c824ef1b64bce5
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2025-12-11 15:28:58 +0200

    tasks react01

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -4,11 +4,15 @@ interface UserDataProps {
   userData: UserType;
 }
 
-export default function User({ userData: { name, email } }: UserDataProps) {
+export default function User({
+  userData: { name, email, isOnline },
+}: UserDataProps) {
   return (
     <>
       <h3>{name}</h3>
       <p>{email}</p>
+      <p>Is user online:{isOnline ? isOnline : "no info"}</p>
+      {/* {isOnline && <p>Is user online: {isOnline}</p>} */}
     </>
   );
 }
