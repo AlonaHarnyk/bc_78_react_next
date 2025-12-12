365ea58b57f10087458f21a343e669fcf39fffde
PolinaGr <polinasoukh@gmail.com>
2025-12-12 16:13:35 +0200

    add button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,14 +5,25 @@ import { useState } from "react";
 
 function App() {
   const [users, setUsers] = useState(data);
+  const [isListVisible, setIsListVisible] = useState(false);
 
   const handleDeleteUser = (id: string) => {
     setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id));
   };
+
+  const showUsers = () => {
+    setIsListVisible(true);
+  };
+
   return (
-    <Section title="List of users">
-      <UserList users={users} onDelete={handleDeleteUser} />
-    </Section>
+    <>
+      <button onClick={showUsers}>Show list of users</button>
+      {isListVisible && (
+        <Section title="List of users">
+          <UserList users={users} onDelete={handleDeleteUser} />
+        </Section>
+      )}
+    </>
   );
 }
 
