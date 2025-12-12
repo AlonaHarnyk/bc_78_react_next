bff9cb85e556e2b85452d1481503ea6b17b68789
Юрий Бедняк <urijbednak@MacBook-Air-Urij.local>
2025-12-12 15:07:06 +0200

    style-info

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,5 +1,6 @@
 import type { UserType } from "../types.tsx";
 import css from "./User.module.css";
+import clsx from "clsx";
 
 interface UserDataProps {
   userData: UserType;
@@ -12,12 +13,23 @@ export default function User({
   console.log(id);
   
   };
+  const getStatusColor = () => {
+    if (isOnline === 'yes') {
+      return css.online;
+    }
+    if (isOnline === 'no') {
+      return css.offline;
+    }
+    return css.noInfo;
+}
+
+const statusStyle = clsx(css.status, getStatusColor())
 
   return (
     <>
       <h3 className={css.title}>{name}</h3>
       <p className={css.paragraph}>{email}</p>
-      <p>Is user online:{isOnline ? isOnline : "no info"}</p>
+      <p className={css.paragraph}>Is user online:<span className={statusStyle}>{isOnline ? isOnline : "no info"}</span></p>
       <button onClick={handleDelete}>Delete</button>
       {/* {isOnline && <p>Is user online: {isOnline}</p>} */}
     </>
