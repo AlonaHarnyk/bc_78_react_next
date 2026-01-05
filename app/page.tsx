f72c7865a2c6120bf0e002e35f9e2dce0af1e461
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2026-01-05 15:15:24 +0200

    add pages and links

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div className={css.container}>
       <ul className={css.navList}>
-        <li className="">
+        <li className={css.navItem}>
           <Link href="/contacts">Contacts</Link>
         </li>
-        <li>
+        <li className={css.navItem}>
           <Link href="/users">Users</Link>
         </li>
       </ul>
---
+++ app/users/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Users() {
+  return (
+    <>
+      <Link href="/">Back To Home</Link>
+      <p> Users</p>
+    </>
+  );
+}
